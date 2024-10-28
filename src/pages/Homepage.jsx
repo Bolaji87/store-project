8d3283d7f40f21279d9ee3c5112f3ac444bd6917
bolaji87 <sakabolaji77@gmail.com>
2024-10-28 14:06:31 +0100

    feat: add categorycard component to homepagge component

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -3,11 +3,19 @@ import React from "react";
 import MensClothing from "../assets/img/men-clothe.png";
 import WomensClothing from "../assets/img/women-clothe.png";
 import Hero from "../components/hero";
+import CategoryCard from "../components/categoryCard";
+import "./styles.css";
 
 function Homepage() {
   return (
     <div>
       <Hero />
+      <hr />
+      <h2>Categories</h2>
+      <div className="cat-cont">
+        <CategoryCard imgSrc={MensClothing} title={"Men's"} />
+        <CategoryCard imgSrc={WomensClothing} title={"Women's"} />
+      </div>
     </div>
   );
 }
