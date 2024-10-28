17c72b956dc6aab2bc747b79bb13033da7232c9a
bolaji87 <sakabolaji77@gmail.com>
2024-10-28 12:45:17 +0100

    feat: create Homepage component

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MensClothing from "../assets/img/men-clothe.png";
+import WomensClothing from "../assets/img/women-clothe.png";
+import Hero from "../components/hero";
+
+function Homepage() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default Homepage;
