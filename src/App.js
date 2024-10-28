780109a1cc916aaad7244b71dd5fe368693f3da5
bolaji87 <sakabolaji77@gmail.com>
2024-10-28 13:51:35 +0100

    feat: map routes in App component and use the Home route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
+import { Approutes } from "./routes";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        {Approutes.map((route) => (
+          <Route
+            key={route.path}
+            exact
+            path={route.path}
+            element={<route.component />}
+          />
+        ))}
+      </Routes>
     </div>
   );
 }
