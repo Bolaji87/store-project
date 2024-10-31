69743a729db0d7b6d0bd959dfa74e3069385df0b
bolaji87 <sakabolaji77@gmail.com>
2024-10-31 10:35:56 +0100

    feat: add filter to select products category

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,36 +1,73 @@
 import React, { useEffect, useState } from "react";
 import { fetchProducts } from "../utils";
+import { ALLOWED_CATEGORIES } from "../routes";
+
 import ProductCard from "../components/productCard";
 function Products() {
+  const [allProducts, setAllProducts] = useState([]);
   const [products, setProducts] = useState([]);
+
+  function handleFilterProducts(productCategory = null) {
+    if (productCategory) {
+      const filterProducts = allProducts.filter(
+        (product) => product.category === productCategory
+      );
+
+      setProducts(filterProducts);
+    } else {
+      setProducts(allProducts);
+    }
+  }
 
   useEffect(function () {
     async function getProducts() {
       const res = await fetchProducts();
       setProducts(res);
-      console.log(res);
+      setAllProducts(res);
     }
     getProducts().catch((e) => console.error("we have an error", e));
   }, []);
   return (
-    <div className="product-card-cont">
-      {products.length > 0 &&
-        products.map(
-          (product) =>
-            product.category !== "jewellery" &&
-            product.category !== "electronics" && (
-              <ProductCard
-                key={product.id}
-                id={product.id}
-                rating={product.rating}
-                img={product.image}
-                price={product.price}
-                description={product.description}
-                categoryName={product.category}
-                productName={product.title}
-              />
-            )
-        )}
+    <div className="products-cont">
+      <div className="category-select">
+        <span
+          onClick={() => handleFilterProducts()}
+          className="select-cat-span"
+        >
+          All
+        </span>
+        <span
+          onClick={() => handleFilterProducts(ALLOWED_CATEGORIES.WOMENS)}
+          className="select-cat-span"
+        >
+          Women's
+        </span>
+        <span
+          onClick={() => handleFilterProducts(ALLOWED_CATEGORIES.MENS)}
+          className="select-cat-span"
+        >
+          Men's
+        </span>
+      </div>
+      <div className="product-card-cont">
+        {products.length > 0 &&
+          products.map(
+            (product) =>
+              product.category !== "jewellery" &&
+              product.category !== "electronics" && (
+                <ProductCard
+                  key={product.id}
+                  id={product.id}
+                  rating={product.rating}
+                  img={product.image}
+                  price={product.price}
+                  description={product.description}
+                  categoryName={product.category}
+                  productName={product.title}
+                />
+              )
+          )}
+      </div>
     </div>
   );
 }
