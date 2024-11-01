9c20a302d2ccb05ea6ca3d24eb95776f411e5813
bolaji87 <sakabolaji77@gmail.com>
2024-11-01 18:16:46 +0100

    feat: add optional url parameter to category

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { fetchProducts } from "../utils";
+import { fetchCategoryProducts, fetchProducts } from "../utils";
 import { ALLOWED_CATEGORIES } from "../routes";
 
 import ProductCard from "../components/productCard";
+import { useParams } from "react-router-dom";
+
 function Products() {
+  const { category } = useParams();
   const [allProducts, setAllProducts] = useState([]);
   const [products, setProducts] = useState([]);
   const [active, setActive] = useState("All");
@@ -20,49 +23,70 @@ function Products() {
     }
   }
 
-  useEffect(function () {
-    async function getProducts() {
-      const res = await fetchProducts();
-      setProducts(res);
-      setAllProducts(res);
-    }
-    getProducts().catch((e) => console.error("we have an error", e));
-  }, []);
+  useEffect(
+    function () {
+      if (!category) {
+        async function getProducts() {
+          const res = await fetchProducts();
+          setProducts(res);
+          setAllProducts(res);
+        }
+        getProducts().catch((e) => console.error("we have an error", e));
+      } else {
+        async function getCategoryProducts() {
+          const res = await fetchCategoryProducts(category);
+          setProducts(res);
+        }
+        getCategoryProducts().catch((e) =>
+          console.error("we have an error", e)
+        );
+      }
+    },
+    [category]
+  );
   return (
     <div className="products-cont">
-      <div className={"category-select"}>
-        <span
-          onClick={() => {
-            handleFilterProducts();
-            setActive("All");
-          }}
-          className={`select-cat-span ${active === "All" ? "cat-active" : ""}`}
-        >
-          All
-        </span>
-        <span
-          onClick={() => {
-            handleFilterProducts(ALLOWED_CATEGORIES.WOMENS);
-            setActive("women's");
-          }}
-          className={`select-cat-span ${
-            active === "women's" ? "cat-active" : ""
-          }`}
-        >
-          Women's
-        </span>
-        <span
-          onClick={() => {
-            handleFilterProducts(ALLOWED_CATEGORIES.MENS);
-            setActive("men's");
-          }}
-          className={`select-cat-span ${
-            active === "men's" ? "cat-active" : ""
-          }`}
-        >
-          Men's
-        </span>
-      </div>
+      {!category ? (
+        <div className="category-select">
+          <span
+            onClick={() => {
+              handleFilterProducts();
+              setActive("All");
+            }}
+            className={`select-cat-span ${
+              active === "All" ? "cat-active" : ""
+            }`}
+          >
+            All
+          </span>
+          <span
+            onClick={() => {
+              handleFilterProducts(ALLOWED_CATEGORIES.WOMENS);
+              setActive("women's");
+            }}
+            className={`select-cat-span ${
+              active === "women's" ? "cat-active" : ""
+            }`}
+          >
+            Women's
+          </span>
+          <span
+            onClick={() => {
+              handleFilterProducts(ALLOWED_CATEGORIES.MENS);
+              setActive("men's");
+            }}
+            className={`select-cat-span ${
+              active === "men's" ? "cat-active" : ""
+            }`}
+          >
+            Men's
+          </span>
+        </div>
+      ) : (
+        <div className="category-select">
+          <span className="cat-active">{category}</span>
+        </div>
+      )}
       <div className="product-card-cont">
         {products.length > 0 &&
           products.map(
