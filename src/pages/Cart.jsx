ed83f3ed7077dbd74fd6d0117e13cf6071daa45a
bolaji87 <sakabolaji77@gmail.com>
2024-11-06 21:08:49 +0100

    feat: add cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -7,12 +7,18 @@ function Cart({ _cartItems }) {
         {_cartItems.cartItems.length > 0 ? (
           _cartItems.cartItems.map((item) => (
             <div key={item.id} className="cart-list-cont">
-              <img src={item.img} alt={item.productNamme} />
+              <img
+                src={item.img}
+                alt={item.productNamme}
+                className="cart-img"
+              />
               <div id="cart-text">
                 <button className="free-shipping">Free shipping</button>
                 <h2 className="cart-item-h2">{item.productNamme}</h2>
-                <h3 className="cart-item-h3"> ${item.price.toFixed(2)}</h3>
-                <h1 className="cart-item-h1"> ${item.price * 1.2}</h1>
+                <h3 className="cart-item-h3">
+                  ${(item.price * 1.2).toFixed(2)}
+                </h3>
+                <h1 className="cart-item-h1"> ${item.price.toFixed(2)}</h1>
                 <h4 className="cart-item-h4">
                   {" "}
                   $
@@ -27,6 +33,16 @@ function Cart({ _cartItems }) {
           <h1>YOU HAVEN'T ADDED ANY ITEMS TO CART</h1>
         )}
       </div>
+      {_cartItems.cartItems.length > 0 && (
+        <div className="cart-summary-cont">
+          <h2>Total : ${_cartItems.totalAmount.toFixed(2)}</h2>
+          <h5> Shipping Cost: $0</h5>
+          <h5>Total Items: {_cartItems.numberOfItems}</h5>
+          <button style={{ alignSelf: "center", width: "100%" }}>
+            Checkout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
