4abe1a3ca0375daad65b3975d1279d3e32103d32
bolaji87 <sakabolaji77@gmail.com>
2024-11-06 21:07:44 +0100

    feat: update app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import { Approutes } from "./routes";
-import { Suspense, useRef } from "react";
-
+import { Suspense, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
+  const initialCartState = {
+    totalAmount: 0,
+    numberOfItems: 0,
+    cartItems: [],
+  };
+  const [cartItems, setCartItems] = useState(initialCartState);
+
   const categoryRef = useRef(null);
   return (
     <div>
-      <Navbar categoryRef={categoryRef} />
+      <Navbar
+        categoryRef={categoryRef}
+        cartItemsCount={cartItems.numberOfItems}
+      />
 
       <Suspense fallback={<h1>Loading....</h1>}>
         <Routes>
@@ -18,7 +27,13 @@ function App() {
               key={route.path}
               exact
               path={route.path}
-              element={<route.component categoryRef={categoryRef} />}
+              element={
+                <route.component
+                  categoryRef={categoryRef}
+                  _cartItems={cartItems}
+                  setCartItems={setCartItems}
+                />
+              }
             />
           ))}
         </Routes>
