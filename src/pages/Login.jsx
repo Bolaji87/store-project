f1ff70b5662e8e7e55edadaec910bcc60f9a8125
bolaji87 <sakabolaji77@gmail.com>
2024-11-08 23:01:39 +0100

    feat: create login component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,29 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import { authenticateUser } from "../utils";
 
-function Login() {
+function Login({ setUser, setIsLogged }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const getUser = async () => {
+      const res = await authenticateUser(email, password);
+      if (res) {
+        setUser(res);
+        setIsLogged(true);
+      } else {
+        setErrorMsg("INVALID EMAIL OR PASSWORD");
+      }
+      console.log("RESPONSE", res);
+    };
+    getUser();
+  };
+
   return (
-    <form className="login-form" action="">
-      <span className="error-span">
-        <label htmlFor="email" className="login-label">
-          Email
-        </label>
-        <input
-          type="text"
-          name="email"
-          className="login-inp"
-          placeholder="Email "
-        />
-        <label htmlFor="password" className="login-label">
-          Password
-        </label>
-        <input
-          type="password"
-          name="password"
-          className="login-inp"
-          placeholder="Email "
-        />
-        <button type="submit">Submit</button>
-      </span>
+    <form className="login-form">
+      <span className="error-span">{errorMsg}</span>
+      <label htmlFor="email" className="login-label">
+        Email
+      </label>
+      <input
+        type="text"
+        name="email"
+        className="login-inp"
+        placeholder="Email "
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label htmlFor="password" className="login-label">
+        Password
+      </label>
+      <input
+        type="password"
+        name="password"
+        className="login-inp"
+        placeholder="Password "
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin} type="submit">
+        Submit
+      </button>
     </form>
   );
 }
