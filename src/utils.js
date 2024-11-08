5b135cd5f02cc1cf7b4432e9503e63af1ebc2890
bolaji87 <sakabolaji77@gmail.com>
2024-11-08 23:02:35 +0100

    feat: create authenticate function

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,11 @@ export async function fetchCategoryProducts(_category) {
   );
   return response.data;
 }
+
+export async function authenticateUser(email, password) {
+  const response = await axios.get(`${BASE_URL}/users`);
+  const authenticate = response.data.find(
+    (user) => user.email === email && user.password === password
+  );
+  return authenticate;
+}
